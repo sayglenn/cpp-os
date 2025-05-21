93677e257b845974caa14d097c97ab2f7b3ecb8f
Say Glenn <glennsay1234@gmail.com>
2025-05-21 21:52:01 +0800

    Build and add gcc cross-compiler

---
+++ env.sh
@@ -0,0 +1,4 @@
+export PREFIX="$HOME/repos/cpp-os/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+export AR=/usr/bin/ar
---
+++ opt/cross/lib/gcc/i686-elf/14.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-14.1.0/configure --target=i686-elf --prefix=/Users/sayglenn/repos/cpp-os/opt/cross --disable-nls --enable-languages=c,c++ --without-headers --with-gmp=/opt/homebrew --with-mpfr=/opt/homebrew --with-mpc=/opt/homebrew --disable-hosted-libstdcxx";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
